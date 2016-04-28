8b1bb9f961054c067874e2bafa40ab603118db72
Bob Fenske <fenske13@gmail.com>
2016-04-28 15:09:24 -0600

    commit

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -4,8 +4,8 @@ from .models import blog
 
 class BlogSpot(forms.ModelForm):
     class Meta:
-        model = blog
-        fields = ['title', 'caption']
+        model = blog[9]
+        fields = ['title', 'caption', 'media']
 
     # class clean_blog(self):
     #     pass #cleans old blog posts from database if more storage is needed
